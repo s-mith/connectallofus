51e88691026ebff41edcd1113a9b1f3577d60d37
Aidan <datbluemushroom@gmail.com>
2022-07-28 02:46:29 -0400

    🚧 Creating a div 'onChildAdded'

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,59 @@
 import '../styles/globals.css'
-import { useAuthState } from 'react-firebase-hooks/auth';
+
+import { PlayerTag } from '../components/PlayerTag';
+
+// firebase imports
 import { auth } from '../firebaseconfig';
 import { db } from '../firebaseconfig';
 import { onAuthStateChanged, signInAnonymously } from 'firebase/auth';
-import { ref, set } from 'firebase/database';
+import { ref, set, onDisconnect, onValue, onChildAdded } from 'firebase/database';
 
 const colors = 
 [
   'red',
   'blue',
-  'yellow'
+  'yellow',
+  'purple',
+  'orange'
 ]
 
 function MyApp ({ Component, pageProps }) 
 {
+  let colorMap =
+  [
+    { 
+      color: 'red' 
+    },
+    { 
+      color: 'blue' 
+    }
+  ];
+
   let playerId;
   let playerRef;
 
-  const color = randomFromArray(colors);
+  function initGame ()
+  {
+    const allPlayersRef = ref(db, `players`);
+    const board = ref(db, `pieces`);
+
+    onValue(allPlayersRef, (snapshot) =>
+    {
+      // Fires whenever a change occurs
+    });
+
+    onChildAdded(allPlayersRef, (snapshot) =>
+    {
+      // Fires whenever a new node is added to the tree
+      const addedPlayer = snapshot.val();
+
+      colorMap.push
+      ({
+        color: addedPlayer.color
+      });
+      console.log(colorMap);
+    });
+  }
 
   // sign in user
   signInAnonymously(auth).catch((error) =>
@@ -40,10 +76,18 @@ function MyApp ({ Component, pageProps })
         playerId = user.uid;
         playerRef = ref(db, 'players/' + playerId);
         
-        set(playerRef, // write data to database on player
+        // write data to database on player
+        set(playerRef,
         {
-          color: 'blue'
+          color: randomFromArray(colors),
+          pieces: [[1, 1], [1, 2]]
         });
+
+        // remove user when disconnected
+        onDisconnect(playerRef).remove();
+
+        // init game
+        initGame();
       }
       else
       {
@@ -52,7 +96,24 @@ function MyApp ({ Component, pageProps })
     }
   })
 
-  return <Component {...pageProps} />
+  return (
+    <>
+
+      // TODO: Fix this map (not working at all)
+      <div>
+        {
+          colorMap.map (item => 
+          {
+            <PlayerTag 
+              color={item.color}
+            />
+          })
+        }
+      </div>
+      <Component {...pageProps} />
+
+    </>
+  )
 }
 
 export default MyApp;
